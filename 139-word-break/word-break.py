b92e93bf4d46732cc0bf446ec963b34a1c975884
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-23 10:28:15 +0530

    Optimize word break logic by removing loop
    Removed unnecessary nested loop for word break check.

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -4,7 +4,5 @@ class Solution:
         dp = [False] * (len(s) + 1)
         dp[0] = True
         
-        for i in range(1, len(s) + 1):
-            for j in range(i):
-                if dp[j] and s[j:i] in word_set:
+       
                     
