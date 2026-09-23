dbb6f2cf5893ecec2fdba09efc7734c05128707d
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-23 10:26:48 +0530

    Refactor reorderList method structure
    Removed early return and added comments for clarity.

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -1,11 +1,7 @@
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         if not head or not head.next:
-            return
-        
-        # Step 1: Find the middle of the linked list
-        slow, fast = head, head.next
-  
+         
             
    
             
