fa75596af7e1fa4eb5e256a25525dd88b3c12581
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-23 10:28:29 +0530

    Refactor wordBreak method by removing unused variables
    Removed initialization of word_set and dp array.

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,8 +1,5 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        word_set = set(wordDict)
-        dp = [False] * (len(s) + 1)
-        dp[0] = True
-        
+      
        
                     
