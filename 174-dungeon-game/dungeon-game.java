eecb5e7316c4601003999531a2034faaf6fcf561
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-23 10:20:03 +0530

    Update dungeon-game.java

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -10,8 +10,5 @@ class Solution {
                     dp[i][j] = Math.max(1, 1 - dungeon[i][j]);
                 } else if (i == m - 1) {
                     dp[i][j] = Math.max(1, dp[i][j + 1] - dungeon[i][j]);
-                } else if (j == n - 1) {
-                    dp[i][j] = Math.max(1, dp[i + 1][j] - dungeon[i][j]);
-                } else {
-                  
+              
    
