866b406b77e972317225210389610f084e4e41a4
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-23 10:20:31 +0530

    Remove unused variables and code in dungeon-game.java

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -1,8 +1,4 @@
 class Solution {
     public int calculateMinimumHP(int[][] dungeon) {
         int m = dungeon.length;
-        int n = dungeon[0].length;
-        int[][] dp = new int[m][n];
-        
-        for (int i = m - 1; i >= 0; i--) {
-        
+      
