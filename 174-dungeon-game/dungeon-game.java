b7c6194ab2bcc511a6e4b5aeeee4251862dc663b
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-23 10:20:21 +0530

    Remove unused code in dungeon-game.java

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -5,7 +5,4 @@ class Solution {
         int[][] dp = new int[m][n];
         
         for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                if (i == m - 1 && j == n - 1) {
-                 
-   
+        
