bf0f77a0e431f71b7fd2383f76516a65defa3783
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-23 10:18:44 +0530

    Refactor minOperations to improve edge case handling
    Handle cases where target is less than or equal to zero.

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
@@ -3,7 +3,4 @@ class Solution:
   def minOperations(self, nums: list[int], x: int) -> int:
     target = sum(nums) - x
     if target < 0:
-      return -1
-    if target == 0:
-      return len(nums)
-
+    
