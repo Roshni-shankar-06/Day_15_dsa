190f20a96dfddcd8219f956c3a52bc394754f9a1
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-23 10:29:35 +0530

    Remove unused loop for word break logic

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -5,6 +5,4 @@ class Solution:
         dp[0] = True
         
         for i in range(1, len(s) + 1):
-            for j in range(i):
-                if dp[j] and s[j:i] in word_set:
-                    
+        
