067d2a72069dd732e05efabae175878d98432a55
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-23 10:15:52 +0530

    Remove DENSE_RANK from salary query

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -4,5 +4,4 @@ WITH RankedSalaries AS (
         d.name AS Department,
         e.name AS Employee,
         e.salary AS Salary,
-        DENSE_RANK() OVER (PARTITION BY e.departmentId ORDER BY e.salary DESC) AS rnk
-    FROM Employee e
+      
